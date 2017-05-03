bd403cff2329e0b3d24ae2ab34b6c19929e91736
Hanif Fakira <fakira.work@gmail.com>
2017-05-03 15:11:11 +0100

    Create about page, static page with new route

--- tests/acceptance/list-rentals-test.js
+++ tests/acceptance/list-rentals-test.js
@@ -10,3 +10,21 @@ test('visiting /', function(assert) {
     assert.equal(currentURL(), '/');
   });
 });
+
+test('should show rentals as the home page', function (assert) {
+});
+
+test('should link to information about the company.', function (assert) {
+});
+
+test('should link to contact information.', function (assert) {
+});
+
+test('should list available rentals.', function (assert) {
+});
+
+test('should filter the list of rentals by city.', function (assert) {
+});
+
+test('should show details for a selected rental', function (assert) {
+});
